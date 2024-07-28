c9e4d9258757d1b8ff049892d18853eaa065907b
zafar <shakirowww@inbox.ru>
2024-07-28 15:03:55 +0600

    change job page and rebuild part 2

---
+++ src/app/job/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import JobCreateComponent from '@/widgets/JobCreateComponent';
+
+function JobPage() {
+  return (
+    <div>
+      <JobCreateComponent />
+    </div>
+  );
+}
+
+export default JobPage;
